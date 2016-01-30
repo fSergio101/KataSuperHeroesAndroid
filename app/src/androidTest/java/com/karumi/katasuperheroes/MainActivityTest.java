edc9da77188cada384941c94ae29e7506d4d8da5
Sergio Martinez <ser.mar.rod.smr@gmail.com>
2016-01-30 12:46:27 +0100

    resolve checkstyle issues added more tests

--- app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/MainActivityTest.java
@@ -18,6 +18,7 @@ package com.karumi.katasuperheroes;
 
 import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
@@ -28,6 +29,7 @@ import com.karumi.katasuperheroes.model.SuperHero;
 import com.karumi.katasuperheroes.model.SuperHeroesRepository;
 import com.karumi.katasuperheroes.recyclerview.RecyclerViewInteraction;
 import com.karumi.katasuperheroes.ui.view.MainActivity;
+import com.karumi.katasuperheroes.ui.view.SuperHeroDetailActivity;
 import it.cosenonjaviste.daggermock.DaggerMockRule;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -38,8 +40,11 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -102,7 +107,7 @@ import static org.mockito.Mockito.when;
             + "experimented on them, once he was disgusted with the results, he returned them to"
             + " Wundagore, disguised as regular mutants."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(
         new SuperHero("Iron Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg",
@@ -111,7 +116,7 @@ import static org.mockito.Mockito.when;
             + "life and escape captivity. Now with a new outlook on life, Tony uses his money "
             + "and intelligence to make the world a safer, better place as Iron Man."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(
         new SuperHero("Wolverine", "https://i.annihil.us/u/prod/marvel/i/mg/9/00/537bcb1133fd7.jpg",
@@ -121,7 +126,7 @@ import static org.mockito.Mockito.when;
                 + "skeleton and claws. Treated like an animal, it took years for him to control"
                 + " himself. Now, he's a premiere member of both the X-Men and the Avengers."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(
         new SuperHero("Hulk", "https://x.annihil.us/u/prod/marvel/i/mg/e/e0/537bafa34baa9.jpg",
@@ -131,7 +136,7 @@ import static org.mockito.Mockito.when;
                 + "Hulk. An all too often misunderstood hero, the angrier the Hulk gets, the "
                 + "stronger the Hulk gets."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(
         new SuperHero("Storm", "https://x.annihil.us/u/prod/marvel/i/mg/c/b0/537bc5f8a8df0.jpg",
@@ -139,7 +144,7 @@ import static org.mockito.Mockito.when;
             "Ororo Monroe is the descendant of an ancient line of African priestesses, all of whom"
                 + " have white hair, blue eyes, and the potential to wield magic."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(new SuperHero("Spider-Man",
         "https://x.annihil.us/u/prod/marvel/i/mg/6/60/538cd3628a05e.jpg", true,
@@ -148,7 +153,7 @@ import static org.mockito.Mockito.when;
             + "a career using his new abilities. Taught that with great power comes great "
             + "responsibility, Spidey has vowed to use his powers to help people."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(
         new SuperHero("Ultron", "https://i.annihil.us/u/prod/marvel/i/mg/9/a0/537bc7f6d5d23.jpg",
@@ -157,7 +162,7 @@ import static org.mockito.Mockito.when;
                 + "Dr. Henry Pym, Ultron is a criminally insane rogue sentient robot dedicated to"
                 + " conquest and the extermination of humanity."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(new SuperHero("BlackPanther",
         "https://i.annihil.us/u/prod/marvel/i/mg/9/03/537ba26276348.jpg", false,
@@ -169,7 +174,7 @@ import static org.mockito.Mockito.when;
             + "will go to extreme measures to achieve his goals and protect the kingdom "
             + "of Wakanda."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(new SuperHero("Captain America",
         "http://x.annihil.us/u/prod/marvel/i/mg/9/80/537ba5b368b7d.jpg", true,
@@ -179,7 +184,7 @@ import static org.mockito.Mockito.when;
             + "intelligence as well as agility, strength, speed, endurance, and reaction time "
             + "superior to any Olympic athlete who ever competed."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(new SuperHero("Winter Soldier",
         "https://i.annihil.us/u/prod/marvel/i/mg/7/40/537bca868687c.jpg", false,
@@ -187,14 +192,14 @@ import static org.mockito.Mockito.when;
             + "hand-to-hand combat and extremely accurate marksman. he is fluent in four languages "
             + "including German and Russian."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(new SuperHero("Captain Marvel",
         "https://x.annihil.us/u/prod/marvel/i/mg/6/30/537ba61b764b4.jpg", false,
         " Ms. Marvel's current powers include flight, enhanced strength, durability and the "
             + "ability to shoot concussive energy bursts from her hands."));
 
-    if (number==superHeroes.size()) return;
+    if (number == superHeroes.size()) return;
 
     superHeroes.add(
         new SuperHero("Iron Fist", "https://i.annihil.us/u/prod/marvel/i/mg/6/60/537bb1756cd26.jpg",
@@ -241,19 +246,49 @@ import static org.mockito.Mockito.when;
 
     RecyclerViewInteraction.<SuperHero>onRecyclerView(withId(R.id.recycler_view))
         .withItems(superHeros)
-        .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>(){
+        .check(new RecyclerViewInteraction.ItemViewAssertion<SuperHero>() {
                  @Override public void check(SuperHero superHero, View view, NoMatchingViewException e) {
-                    matches(hasDescendant(withText(superHero.getName()))).check(view,e);
+                    matches(hasDescendant(withText(superHero.getName()))).check(view, e);
                  }
                }
         );
-
 
     //onView(withText("Scarlet Witch")).check(matches(isDisplayed()));
     //onView(withText("Iron Man")).check(matches(isDisplayed()));
     //onView(withText("Wolverine")).check(matches(isDisplayed()));
     //onView(withText("Hulk")).check(matches(isDisplayed()));
 
+  }
+
+  @Test public void showsTappedElement() {
+    int heroToTest = 8;
+    final List<SuperHero> superHeros = givenThereAreTenSuperHeroes();
+    givenSuperHeroByName(superHeros.get(heroToTest));
+
+    startActivity();
+
+    onView(withId(R.id.recycler_view)).perform(
+        RecyclerViewActions.actionOnItemAtPosition(heroToTest, click()));
+
+    onView(withText(superHeros.get(heroToTest).getDescription())).check(matches(isDisplayed()));
+  }
+
+  @Test public void showsTappedElementUsingIntent() {
+    List<SuperHero> superHeroes = givenThereAreTenSuperHeroes();
+    int superHeroIndex = 1;
+    givenSuperHeroByName(superHeroes.get(superHeroIndex));
+    startActivity();
+
+    onView(withId(R.id.recycler_view)).perform(
+        RecyclerViewActions.actionOnItemAtPosition(superHeroIndex, click()));
+
+    SuperHero superHeroSelected = superHeroes.get(superHeroIndex);
+    intended(hasComponent(SuperHeroDetailActivity.class.getCanonicalName()));
+    intended(hasExtra("super_hero_name_key", superHeroSelected.getName()));
+  }
+
+  private void givenSuperHeroByName(SuperHero superHero) {
+    when(repository.getByName(superHero.getName())).thenReturn(superHero);
   }
 
   private List<SuperHero> givenThereAreTenSuperHeroes() {
