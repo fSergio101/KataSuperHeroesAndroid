6e3cd27c26845088f66c58c8c89ac5d1c7172447
Sergio Martinez <ser.mar.rod.smr@gmail.com>
2016-01-30 13:07:35 +0100

    added second activity Test

--- app/src/androidTest/java/com/karumi/katasuperheroes/SuperHeroDetailActivityTest.java
+++ app/src/androidTest/java/com/karumi/katasuperheroes/SuperHeroDetailActivityTest.java
@@ -1,18 +1,30 @@
 package com.karumi.katasuperheroes;
 
+import android.content.Intent;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.intent.rule.IntentsTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 import com.karumi.katasuperheroes.di.MainComponent;
 import com.karumi.katasuperheroes.di.MainModule;
+import com.karumi.katasuperheroes.model.SuperHero;
 import com.karumi.katasuperheroes.model.SuperHeroesRepository;
-import com.karumi.katasuperheroes.ui.view.MainActivity;
 import com.karumi.katasuperheroes.ui.view.SuperHeroDetailActivity;
 import it.cosenonjaviste.daggermock.DaggerMockRule;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.karumi.katasuperheroes.matchers.ToolbarMatcher.onToolbarWithTitle;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Sergio Martinez Rodriguez
@@ -37,8 +49,33 @@ import org.mockito.Mock;
 
   @Mock SuperHeroesRepository repository;
 
-  //private SuperHeroDetailActivity startActivity() {
-  //  return activityRule.;
-  //}
+  private SuperHeroDetailActivity startActivity(SuperHero superHero) {
+    Intent intent = new Intent();
+    intent.putExtra("super_hero_name_key", superHero.getName());
+    return activityRule.launchActivity(intent);
+  }
+
+  @Test public void showsTappedElementUsingIntent() {
+    SuperHero superHero = createMockSuperHero();
+    givenSuperHeroByName(superHero);
+    startActivity(superHero);
+
+    onToolbarWithTitle(superHero.getName()).check(matches(isDisplayed()));
+    onView(withText(superHero.getDescription())).check(matches(isDisplayed()));
+  }
+
+  private SuperHero createMockSuperHero() {
+    return new SuperHero("Iron Man", "https://i.annihil.us/u/prod/marvel/i/mg/c/60/55b6a28ef24fa.jpg",
+        true, "Wounded, captured and forced to build a weapon by his enemies, billionaire "
+        + "industrialist Tony Stark instead created an advanced suit of armor to save his "
+        + "life and escape captivity. Now with a new outlook on life, Tony uses his money "
+        + "and intelligence to make the world a safer, better place as Iron Man.");
+  }
+
+  private void givenSuperHeroByName(SuperHero superHero) {
+    when(repository.getByName(superHero.getName())).thenReturn(superHero);
+  }
+
+
 
 }
